6966b63fbc18ea32a96d9ee56cda6f4e4d1574bd
小饼干 <2200469734@qq.com>
2025-09-20 16:02:22 +0800

    Create auto.js

---
+++ bgtp/auto.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const imageDir = './images';
+const images = fs.readdirSync(imageDir).filter(file => file.endsWith('.png'));
+
+const output = {
+    images: images.map(img => `images/${img}`),
+    lastUpdated: new Date().toISOString()
+};
+
+fs.writeFileSync('image-list.json', JSON.stringify(output, null, 2));
+console.log('图片列表已更新');
